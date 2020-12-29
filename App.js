43fbc8ac0d345ed1f30756d925794e3ada8d3e24
nooorfatimaa <noorfatima9826@gmail.com>
2020-12-29 18:28:30 +0500

    second

--- App.js
+++ App.js
@@ -1,21 +1,119 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { View, Text, StyleSheet, Pressable, Button, TouchableOpacity, FlatList } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Ionicons, FontAwesome5 } from '@expo/vector-icons';
+
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const WorldStatsScreen = ({navigation}) => {
+  return(
+    <View style={styles.container}>
+      <View style={{alignItems: 'center'}}><Text style={{fontWeight: 'bold', fontSize: '30', color: 'grey'}}>WORLD STATISTICS</Text></View>
+      <Button
+        title="Go to country list"
+        onPress={() => navigation.navigate("Countries List")}
+      ></Button>
+    </View>
+  );
+}
+
+const CountryListScreen = ({navigation}) => {
+  return(
+    <View>
+      <Button
+        title="Go to country list"
+        onPress={() => navigation.navigate("Countries List")}
+      ></Button>
+    </View>
+  );
+}
+
+const CountryStatsScreen = ({navigation}) => {
+  return(
+    <View>
+      <Button
+        title="Go to country list"
+        onPress={() => navigation.navigate("Countries List")}
+      ></Button>
+    </View>
+  );
+}
+
+const FavouriteListScreen = ({navigation}) => {
+  return(
+    <View>
+      <Button
+        title="Go to country list"
+        onPress={() => navigation.navigate("Countries List")}
+      ></Button>
+    </View>
+  );
+}
+
+const CountryStack = () => {
+  return (
+    <Stack.Navigator
+      //initialRouteName={"Start"} 
+      screenOptions={{
+        headerTintColor: "grey",
+        headerStyle: {
+          backgroundColor: "lightblue"
+        }
+      }}>
+      <Stack.Screen name="Countries List" component={CountryListScreen} />
+      <Stack.Screen name="Country Statistics" component={CountryStatsScreen} />
+    </Stack.Navigator>
+  );
+}
+
+const FavouriteStack = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerTintColor: "grey",
+        headerStyle: {
+          backgroundColor: "lightblue"
+        }
+      }}>
+      <Stack.Screen name="Favourites" component={FavouriteListScreen} />
+      <Stack.Screen name="Country Statistics" component={CountryStatsScreen} />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator
+      //openByDefault={true}
+      //drawerType="slide"
+      //drawerStyle={{
+        //backgroundColor: 'lightyellow',
+        //width: 200,
+      //}}>
+      >
+      <Drawer.Screen name="Home" component={WorldStatsScreen} />
+      <Drawer.Screen name="Countries List" component={CountryStack} />
+      <Drawer.Screen name="Favourites" component={FavouriteStack} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'lightblue',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
